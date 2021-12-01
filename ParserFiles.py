2e3565db21bd4c96b57dfc5181cedeb6c82bcbab
DENNKA <spiridonov.denis34@gmail.com>
2021-12-01 21:22:57 +0400

    Add data and new extension

--- ParserFiles.py
+++ ParserFiles.py
@@ -7,7 +7,7 @@ class ParserFiles():
     def __init__(self):
         self.video_extensions = ['mkv', 'avi', 'mp4']
         self.subtitle_extensions = ['ass', 'srt']
-        self.audio_extensions = ['mka']
+        self.audio_extensions = ['mka', 'ac3']
         self.font_extensions = ['ttf', 'ttc', 'otf']
 
         zip1 = zip(self.video_extensions + self.subtitle_extensions + self.audio_extensions + self.font_extensions,
@@ -20,10 +20,11 @@ class ParserFiles():
 
         self.subtitles_folder_names = ['sub']
 
-    def files_to_episodes(self, files, root):
+    def files_to_episodes(self, files, root, data=None):
         """
         files: list of files (TORRENT_NAME/file1)
         root: torrent download location (/disk/TORRENTS_FOLDER)
+        data: list data. Replacing path with data (path[i] = data[i])
         Returns dict (path is joined root and file path)
         {
             episode_number : { video : path, subtitle : { group_name1 : path, group_name2 : path }, audio : path }
@@ -39,19 +40,23 @@ class ParserFiles():
 
         subtitle_in_folder = 1 if len(subtitles_folders) else 0
 
-        for path in paths:
+        for i, path in enumerate(paths):
             file_type = self.extension_dict[path.suffix.lstrip('.')]
+            if data is None:
+                append_data = str(root / path)
+            else:
+                append_data = data[i]
             if file_type != "fonts":
                 file_episode = int(re.search(r'\d+', path.stem).group())
                 if file_type == "subtitle":
                     try:
-                        files_dict[file_episode][file_type][str(Path(*path.parts[1 + subtitle_in_folder:-1]))] = str(root / path)
+                        files_dict[file_episode][file_type][str(Path(*path.parts[1 + subtitle_in_folder:-1]))] = append_data
                     except TypeError:
                         files_dict[file_episode][file_type] = defaultdict()
-                        files_dict[file_episode][file_type][str(Path(*path.parts[1 + subtitle_in_folder:-1]))] = str(root / path)
+                        files_dict[file_episode][file_type][str(Path(*path.parts[1 + subtitle_in_folder:-1]))] = append_data
                         pass
                 else:
-                    files_dict[file_episode][file_type] = str(root / path)
+                    files_dict[file_episode][file_type] = append_data
             else:
                 files_dict[file_type][str(Path(*path.parts[1 + subtitle_in_folder:-2]))].append(str(root / path))
         return files_dict
