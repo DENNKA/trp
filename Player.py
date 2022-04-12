60bcc00ac1f1a1a340259fd5f3e1b7c3ad3f9780
DENNKA <spiridonov.denis34@gmail.com>
2022-04-12 22:08:46 +0400

    Cleanup

--- Player.py
+++ Player.py
@@ -1,13 +1,9 @@
 import io
-import mpv
 from subprocess import Popen, PIPE
-
-def log_handler(level, prefix, text):
-    print("PLAYER: ", level, prefix, text)
 
 class Player():
     def __init__(self):
-        self.player = mpv.MPV(log_handler=log_handler, input_default_bindings=True, input_vo_keyboard=True, osc=True)
+        pass
 
     # in seconds
     def _time_to_return_code(self, position, duration):
@@ -19,12 +15,7 @@ class Player():
         return sum([a * b for a, b in zip(ftr, map(int, string.split(':')))])
 
     def play(self, file, subtitles=None):
-        # process = Popen(['mpv', file, "--term-status-msg='${time-pos} / ${duration}'"], stdout=PIPE, stderr=PIPE, bufsize=1, universal_newlines=True)
         process = Popen(['mpv', file, "--term-status-msg=${time-pos} / ${duration}"], stdout=PIPE, stderr=PIPE, universal_newlines=True)
-        # stdout, stderr = process.communicate()
-        # while True:
-            # out = process.stdout.read(20)
-            # print(out)
         error = 1
         while True:
             line = process.stderr.readline()
