c943e8c390af5fd05d08b125b2d1a1830b237a83
DENNKA <spiridonov.denis34@gmail.com>
2022-04-13 18:57:33 +0400

    setup tests for github actions

--- trp/tests/test_torrent_client.py
+++ trp/tests/test_torrent_client.py
@@ -11,6 +11,7 @@ cfg = configparser.ConfigParser()
 cfg.read("settings.cfg")
 @pytest.mark.parametrize(
         ('torrent_client'), [(x) for x in TorrentClients(cfg, None).get_all_classes(False)])
+@pytest.mark.high_dependency()
 class TestTorrentClient:
     def test_add_update_remove_torrent(self, torrent_client):
         anime = Anime()
